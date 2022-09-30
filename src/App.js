d365b5f339d99e36bf2ffc53ec6d558f1da8ac20
abdulrehman <abdulrehman@tcs.com.pk>
2022-09-30 12:30:07 +0500

    routes, integrating pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Layout from "./Component/Layout";
+// import Layout from "./Component/Layout";
 import Events from "./Pages/EventPage";
 import Landing from "./Pages/LandingPage";
+import { Routes } from "react-router";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Landing />
-      </Layout>
-      <Events />
+      {/* <Layout> */}
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/events" element={<Events />} />
+        </Routes>
+      </Router>
+      {/* <Landing /> */}
+      {/* </Layout> */}
     </div>
   );
 }
--- src/Pages/EventPage.js
+++ src/Pages/EventPage.js
@@ -1,12 +1,77 @@
 import React from "react";
-import Landing from "./LandingPage";
+import { FiSettings } from "react-icons/fi";
 
 const Events = () => {
   return (
     <div>
-     <div className="event-image">
-         {/* <img src="event-image.svg" alt="" /> */}
-     </div>
+      <div className="event-details">
+        <button className="event-cl">Cancel</button>
+        <div className="event-icon">
+          <FiSettings />
+        </div>
+      </div>
+      <div className="section">
+        <div className="sec-left">
+          <section className="w-full max-w-lg px-3 mx-auto lg:px-0 lg:max-w-none">
+            <h1 className="hidden font-semibold text-left lg:block mb-9 text-text-primary text-md md:text-5xl mytitle">
+              Create your event
+            </h1>
+            <p className="">🎉 My event is called</p>
+            <input name="eventName" maxLength="30" className="" />
+            <p className="">🗓 It starts at</p>
+            <div className="">
+              <div className="react-datepicker-wrapper">
+                <div className="react-datepicker__input-container">
+                  <input type="text" id="startTime" className="w-full" />
+                </div>
+              </div>
+            </div>
+            <p className="">
+              🏁 It ends at
+              <span className="font-light text-grey-500"> (optional)</span>
+            </p>
+            <div className="">
+              <div className="react-datepicker-wrapper">
+                <div className="react-datepicker__input-container">
+                  <input type="text" className="w-full" />
+                </div>
+              </div>
+            </div>
+            <p className="">
+              📍 It’s happening at
+              <span className="font-light text-grey-500"> (optional)</span>
+            </p>
+            <div className="mb-2">
+              <input
+                name="location"
+                type="text"
+                autoComplete="off"
+                role="combobox"
+                aria-autocomplete="list"
+                aria-expanded="false"
+                placeholder="Location"
+              />
+            </div>
+            <p className="">
+              🔗 Add a URL link
+              <span className="font-light text-grey-500"> (optional)</span>
+            </p>
+            <input name="url" placeholder="URL" className="" />
+            <p className="">
+              ✏️ Description
+              <span className="font-light text-grey-500"> (optional)</span>
+            </p>
+            <textarea
+              rows="4"
+              name="description"
+              style={{ height: "60px !important" }}
+            ></textarea>
+          </section>
+        </div>
+        <div className="sec-right">
+          <img className="sec-img" src="celebration.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
